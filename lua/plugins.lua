c54dd3e50d50487538a6d4af5cbf18ad8017c45c
George <dyejeekis@gmail.com>
2025-02-01 16:39:48 +0200

    fix indentation

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -493,27 +493,27 @@ return {
 	},
 
 	{
-	"mikavilpas/yazi.nvim",
-	event = "VeryLazy",
-	keys = {
-		{
-			"<leader>cf",
-			mode = { "n", "v" },
-			"<cmd>Yazi<cr>",
-			desc = "Open yazi at the current file",
+		"mikavilpas/yazi.nvim",
+		event = "VeryLazy",
+		keys = {
+			{
+				"<leader>cf",
+				mode = { "n", "v" },
+				"<cmd>Yazi<cr>",
+				desc = "Open yazi at the current file",
+			},
+			{
+				"<leader>cw",
+				"<cmd>Yazi cwd<cr>",
+				desc = "Open the file manager in nvim's working directory",
+			},
+			{
+				"<leader>cr",
+				"<cmd>Yazi toggle<cr>",
+				desc = "Resume the last yazi session",
+			},
 		},
-		{
-			"<leader>cw",
-			"<cmd>Yazi cwd<cr>",
-			desc = "Open the file manager in nvim's working directory",
-		},
-		{
-			"<leader>cr",
-			"<cmd>Yazi toggle<cr>",
-			desc = "Resume the last yazi session",
-		},
-	},
-	opts = {
+		opts = {
 			open_for_directories = false,
 			keymaps = {
 				show_help = "<f1>",
